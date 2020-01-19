18ea0b118052d2abdcec4f4c83f6a8cdef247bf6
hanisathish <hanisathish@github.com>
2020-01-19 11:07:07 +0530

    uncomment

--- resources/views/frontend/list.blade.php
+++ resources/views/frontend/list.blade.php
@@ -34,18 +34,7 @@
                 <li class="{{$activetab}}"><a href="{!!route('frontend.list',array($categoryData->id))!!}" class="hreftabs">{{$categoryData->title}}</a></li>
               @endforeach
             @endif    
-            <!-- <li>Mobiles</li>
-            <li>Electronics & Appliances</li>
-            <li>Cars</li>
-            <li>Bikes</li>
-            <li>Furniture</li>
-            <li>Pets</li>
-            <li>Books, Sports & Hobbies</li>
-            <li>Fashion</li>
-            <li>Kids</li>
-            <li>Services</li>
-            <li>Jobs</li>
-            <li>Real Estate</li> -->
+            
           </ul>
             <a class="w3ls-ads" href="all-classifieds.html">View all Ads</a>
           </div>
@@ -70,9 +59,9 @@
                     @if($categoryData->id == $category->id)
                       @foreach($categoryData->subCategory as $subcategory)
                         
-                        <!-- @if($categoryData->id === $subcategory->parent_id) -->
+                        
                           <li><a href="{!!route('frontend.detail.list',array($category->id,$subcategory->id))!!}">{!!$subcategory->title!!} <span class="badge">{!!$subcategory->itemCount!!}</span></a></li>
-                        <!-- @endif -->
+                        
                       @endforeach
                     @endif  
                     @endforeach
@@ -95,44 +84,7 @@
     </div>
   </div>
 
-<!-- 
-  <div class="row">
-  	  @foreach($items as $item)
-	  <div class="col-sm-6 col-md-4">
-	    <div class="thumbnail">
-          @if(empty($item->image))
-	      <img alt="{!!$item->title!!}" src="http://placehold.it/100X200?text=Image" data-holder-rendered="true" style="height: 200px; width: 100%; display: block;">
-	      @else
-          <img alt="{!!$item->title!!}" src="{!!asset('uploads/'.$item->image) !!}" data-holder-rendered="true" style="height: 200px; width: 100%; display: block;">
-          @endif
-          <div class="caption">
-	        <h3>{!! $item->title!!}</h3>	        
-	        <p>{!! str_limit($item->description, 100)!!}</p>
-            <p>Category: {!! $item->category->title !!}    </p>
-	        <p>           
-    			{!! Html::linkRoute('frontend.item.show', 'View', array($item->id) ,['class' => "btn btn-primary"]) !!} 
-                @if(Auth::user())
-                    @if(Auth::user()->id == $item->user_id)
-                        {!! Html::linkRoute('frontend.post.edit', 'Edit', array($item->id) ,['class' => "btn btn-default"]) !!}     	                        
-                    @endif
-                @endif
-                 
-            </p>
-	      </div>
-	    </div>
-	   </div>
-
-	   @endforeach
-
-	    <hr>
-
-        <div class="row text-center">
-            <div class="col-lg-12">
-               {!! $items->links() !!}      
-            </div>
-        </div>
-  
-  </div> -->
+ 
 
 @endsection
 
